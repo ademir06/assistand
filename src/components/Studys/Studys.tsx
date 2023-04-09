db562a3f92a6ead45f30db46d92629c6edf7158b
Ademir__Dev <gaparov866@gmail.com>
2023-04-09 16:09:12 +0600

    pp

--- src/components/Studys/Studys.tsx
+++ src/components/Studys/Studys.tsx
@@ -14,14 +14,17 @@ const Studys = ({dark}: any) => {
                     </div>
                     <div className="study-one__text">
                         <h1>Чем мы занимаемся?</h1>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget neque, dignissim et feugiat
-                            elit augue in suspendisse egestas. Dictum vestibulum mi et sed nunc, orci fermentum
-                            vestibulum, morbi. Et neque, adipiscing sapien sem senectus praesent aenean consequat.
-                            Aenean facilisi turpis aliquet fringilla. Nunc sem felis sed interdum feugiat mattis elit,
-                            sollicitudin. Quam pharetra rhoncus risus, cursus id elementum aliquet. Nullam turpis arcu
-                            malesuada arcu interdum placerat nisi, lobortis. </p>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget neque, dignissim et feugiat <br/>
+                            elit augue in suspendisse egestas. Dictum vestibulum mi et sed nunc, orci fermentum <br/>
+                            vestibulum, morbi. Et neque, adipiscing sapien sem senectus praesent aenean consequat. <br/>
+                            Aenean facilisi turpis aliquet fringilla. Nunc sem felis sed interdum feugiat mattis elit, <br/>
+                            sollicitudin. Quam pharetra rhoncus risus, cursus id elementum aliquet. Nullam turpis arcu <br/>
+                            malesuada arcu interdum placerat nisi, lobortis.
+                        </p>
                     </div>
                 </div>
+
             </div>
         </div>
     );
